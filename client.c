a06a5d9be56ce72bf7bb18040459ef049b1aa491
Никитос <nikitos@MacBook-Air-Tana.local>
2025-12-21 23:36:09 +0300

    Delete unneunnecessary libraries

--- client.c
+++ client.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <poll.h>
 #include <errno.h>
 #include <netdb.h>
--- server.c
+++ server.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <ifaddrs.h>
